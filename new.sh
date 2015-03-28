be32683562d1589438cf3dd00da4e509bfe9aa71
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2015-03-28 15:38:29 -0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -30,7 +30,6 @@ cat << EOF > $default_folder/$name/$name.html
 EOF
 
 cat << EOF > $default_folder/$name/$name.styl
-// Import this to get access to palette and variables
 @import '../../styles/base'
 EOF
 
